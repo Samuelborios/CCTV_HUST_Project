b4bd791a02db4296c8f3d6896a184365d6221ff6
Samuel Borios <samuel.borios@grenoble-inp.org>
2025-04-01 16:33:13 +0700

    netlify added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import netlify from '@astro/adapters/netlify';
 
+import db from '@astrojs/db';
+
+import netlify from '@astrojs/netlify';
+
+// https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
+  integrations: [db()],
+  adapter: netlify()
 });
